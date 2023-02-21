26bbd916e78013757213bdea8d02e95485a1fb13
LukazDs <lukazds2016@gmail.com>
2023-02-21 13:19:51 -0300

    feat: add design menu

--- src/app.js
+++ src/app.js
@@ -18,6 +18,8 @@ function makePokemons(pokemons) {
   const container = document.querySelector(".container");
 
   for (let i = 0; i < pokemons.length; i++) {
+    const name = pokemons[i].name[0].toUpperCase() + pokemons[i].name.slice(1);
+
     container.innerHTML += `<div class="pokemon">
         <img 
             class="img-pokemon" 
@@ -26,7 +28,7 @@ function makePokemons(pokemons) {
             }.png" 
             alt="pokemon" 
         />
-        <p class="name-pokemon">${pokemons[i].name}</p>
+        <p class="name-pokemon">${name}</p>
     </div>`;
   }
 }

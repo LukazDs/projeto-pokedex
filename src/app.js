9f92dbf8831369dcd9dd9af0f1f9e854c455243d
LukazDs <lukazds2016@gmail.com>
2023-02-22 16:53:32 -0300

    feat: change choice pokemon

--- src/app.js
+++ src/app.js
@@ -4,6 +4,35 @@ function getPokemon() {
   function carregarPokemons(dados) {
     pokemons = dados.data;
 
+    function makePokemons(pokemons) {
+      const container = document.querySelector(".container");
+
+      for (let i = 0; i < pokemons.length; i++) {
+        const name =
+          pokemons[i].name[0].toUpperCase() + pokemons[i].name.slice(1);
+
+        container.innerHTML += `
+          <button 
+            class="pokemon"
+            onclick="toCapture(this)"
+          >
+              <img 
+                class="capture" 
+                src="https://w7.pngwing.com/pngs/213/264/png-transparent-poke-ball-coloring-book-drawing-pokemon-sun-and-moon-pokemon-comics-angle-text.png"
+                alt="poke"
+              />
+              <img 
+                  class="img-pokemon" 
+                  src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                    i + 1
+                  }.png" 
+                  alt="pokemon" 
+              />
+              <p class="name-pokemon">${name}</p>
+          </button>`;
+      }
+    }
+
     makePokemons(pokemons.results);
   }
 
@@ -14,23 +43,15 @@ function getPokemon() {
   promise.then(carregarPokemons);
 }
 
-function makePokemons(pokemons) {
-  const container = document.querySelector(".container");
+function toCapture(element) {
+  element.querySelector(".capture").classList.toggle("selected");
+  const imgSelected = element.querySelector(".selected");
 
-  for (let i = 0; i < pokemons.length; i++) {
-    const name = pokemons[i].name[0].toUpperCase() + pokemons[i].name.slice(1);
+  const imgUrl = imgSelected
+    ? "https://w7.pngwing.com/pngs/620/521/png-transparent-poke-ball-pokemon-pokemon-rim-mobile-phones-pokemon.png"
+    : "https://w7.pngwing.com/pngs/213/264/png-transparent-poke-ball-coloring-book-drawing-pokemon-sun-and-moon-pokemon-comics-angle-text.png";
 
-    container.innerHTML += `<div class="pokemon">
-        <img 
-            class="img-pokemon" 
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-              i + 1
-            }.png" 
-            alt="pokemon" 
-        />
-        <p class="name-pokemon">${name}</p>
-    </div>`;
-  }
+  element.querySelector(".capture").setAttribute("src", imgUrl);
 }
 
 getPokemon();
